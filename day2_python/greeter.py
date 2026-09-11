64b69ef2b714cafd8f89f88e9c804fb9bbe10eb7
Owen McDonough <owenmcdonough@Owens-MacBook-Air.local>
2026-09-10 22:09:50 -0700

    Finished the greeter.py script

--- day2_python/greeter.py
+++ day2_python/greeter.py
@@ -1,9 +1,19 @@
 print("What's your name?")
 name = input("Type your name here: ")
-if name != str:
-	print("Please input your name as a String.")
-print("What year were you born? ")
-byear = 2026 - int(input("Type your birth year here: "))
-if byear != int:
-	print("Please input your birth year as an integer.")
-print(f"Hello, {name}! You are {byear} years old.")
+if name.isalpha():
+	print("What year were you born? ")	
+	byear = input("Type your birth year here: ")
+	try:
+		byear_as_int = int(byear)
+		age = 2026 - byear_as_int
+		if age > 100:
+			print("It's unlikely you're over 100 years old. Input your real birthyear please.")
+		else:
+			if age < 1:
+				print("I don't think you're old enough to type yet, or perhaps exist? Input your real birthyear please.")
+			else:
+				print(f"Hello, {name}! You are {age} years old.")
+	except ValueError:
+		print("Please input your birth year as an integer.")
+else:
+	print("Please input your name with only alphabet characters.")
